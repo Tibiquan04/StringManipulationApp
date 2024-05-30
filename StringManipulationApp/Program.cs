8df79f77344f5b268d40b2f443ee620f28732c4e
Tibiquan04 <tibiquan2004@gmail.com>
2024-05-30 02:20:17 +0200

    Add project files.

---
+++ StringManipulationApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace StringManipulationApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "Hello World";
+            string result1 = RemoveFirstCharacter(input);
+            string result2 = RemoveLastCharacter(result1);
+
+            Console.WriteLine(result2);
+        }
+
+        static string RemoveFirstCharacter(string input)
+        {
+            if (string.IsNullOrEmpty(input)) return input;
+            return input.Substring(1);
+        }
+
+        static string RemoveLastCharacter(string input)
+        {
+            if (string.IsNullOrEmpty(input)) return input;
+            return input.Substring(0, input.Length - 1);
+        }
+    }
+}
